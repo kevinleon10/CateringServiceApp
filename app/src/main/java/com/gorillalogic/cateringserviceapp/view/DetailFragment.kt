dcc19cb89b5b574d62a8568aa50e8b1fcdcbfeec
kevinleon10 <lskevin97@hotmail.com>
2020-05-24 17:06:07 -0600

    Removed unused code.

--- app/src/main/java/com/gorillalogic/cateringserviceapp/view/DetailFragment.kt
+++ app/src/main/java/com/gorillalogic/cateringserviceapp/view/DetailFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.gorillalogic.cateringserviceapp.R
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
